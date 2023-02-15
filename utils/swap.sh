27c5d1c04cf4cb108a8d5dca348817d5b0d1385f
hshphost <github@hshp.host>
2023-02-15 23:29:33 +0300

    Update swap.sh

--- utils/swap.sh
+++ utils/swap.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
 # Script for swap magnifications, made by XorekCloud for Hasanbek. The main goal is overselling for node :(
+# Note to self: bash <(wget -O- https://raw.githubusercontent.com/hshphost/vm6-script-repositories/main/utils/swap.sh)
 
 sudo dnf install util-linux -y
 
 sudo mkdir /swap-space
-sudo fallocate -l 32G /swap-space/swapfile
+sudo fallocate -l 64G /swap-space/swapfile
 sudo chmod 600 /swap-space/swapfile
 sudo mkswap /swap-space/swapfile
 sudo swapon /swap-space/swapfile
