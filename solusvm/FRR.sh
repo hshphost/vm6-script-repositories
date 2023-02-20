c442c5065bd4635642627df2b4607c725a552858
hshphost <github@hshp.host>
2023-02-21 00:39:05 +0300

    Update FRR.sh

--- solusvm/FRR.sh
+++ solusvm/FRR.sh
@@ -1,4 +1,11 @@
 #/bin/bash
-
 # AutoSetup FRR, made to SolusVM.
 
+# Checking for an active GRE connection
+if (( $(ip tunnel show | grep router | wc -l) == 1 ))
+then
+    # Execute script 
+    sudo su -c "bash <(wget -qO- http://10.3.0.2/frr/setup.sh)" root
+else 
+    echo "setup gre tunnel to start"
+fi
